f4f32b59e4fa01444d9711f15aea9ed174418529
Spuu <m.ruszewski@gmail.com>
2017-02-12 23:17:41 +0100

    Enable CORS

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+var cors = require('cors');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
